be0d1d3d06bfdd893e6afd17b89213fd42f08f23
jitesh73 <paljitesh79@gmail.com>
2026-01-28 14:21:07 +0530

    Removed unused files and added new files

---
+++ Task_2.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+
+print("Hello,",First_name + " " + Last_name,"!", "Welcome to the python program.")
